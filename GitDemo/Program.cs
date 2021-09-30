d9251a3538fbf8640ece3beaaa38ae17b7453027
Craig Franceschetti <craigfranceschetti@gmail.com>
2021-09-30 14:05:16 -0500

    Add project files.

---
+++ GitDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    class Program
+    {
+
+        // Create/Clone a repo
+
+        // Commits
+
+        // Pull
+
+        // Push
+
+        // Pull Request
+
+        // Branches
+
+        // Merge
+
+        // Rebase - optional
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
